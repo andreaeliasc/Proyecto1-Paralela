e77d4cb1fdca136da75b15c643bf430e8768f3c4
Diego Estrada <diegoestrada@MacBook-Air-de-Diego.local>
2022-03-03 20:07:57 -0600

    starting

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+Authors:
+Andrea Elias 17048
+Sara Zavala 18893
+Diego Estrada 18540
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
